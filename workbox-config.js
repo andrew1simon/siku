dc51b2bb0d2a6070db80e67e94a808e37bb6fca8
Andrew Simon <46216553+andrew1simon@users.noreply.github.com>
2023-07-14 00:25:12 +0300

    final rel

--- workbox-config.js
+++ workbox-config.js
@@ -1,30 +1,12 @@
 module.exports = {
 	globDirectory: './',
 	globPatterns: [
-		'**/*.{html,png,jpg,jpeg,css,json}'
+		'**/*.{html,png,jpg,jpeg,css,json,ico}'
 	],
+	
 	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
 	]
-	,
-	runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/font\-awesome\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'fontaw-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ],
-		cleanupOutdatedCaches: true
-
 };
